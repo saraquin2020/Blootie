311f7d94339988ea7ae36243fee338528af67ee5
Sara <saraquin2020@gmail.com>
2026-06-09 15:29:23 -0500

    Arreglando carga de archivos multimedia

--- blootie_project/wsgi.py
+++ blootie_project/wsgi.py
@@ -1,5 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blootie_project.settings')
-application = get_wsgi_application()
+
+# Esto obliga a Render a mostrar el diseño Y la carpeta media (fotos/audios)
+application = Cling(MediaCling(get_wsgi_application()))
