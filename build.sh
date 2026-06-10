f75b70c493707cf67a7c8bded2d7b9c952bb0098
Sara <saraquin2020@gmail.com>
2026-06-09 21:51:04 -0500

    Reparando modulo de actividades manteniendo la carga de cuentos exitosa

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py migrate
 python manage.py loaddata usuarios.json
 python manage.py loaddata perfiles.json
 python manage.py loaddata cuentos.json
+
+# ESTA LÍNEA SOLUCIONA LAS ACTIVIDADES: Trae los datos de control faltantes sin romper los cuentos
+python manage.py loaddata todo_el_localhost.json
